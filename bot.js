0b5ae61b9871a9b6157d41e27af7dfa4792d9eff
TheKingPro <45007892+TheKingPro@users.noreply.github.com>
2018-12-29 02:06:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,6 @@ client.on('message', message => {
     if(!args.join(" ")) return message.channel.send(`**يرجي كتابة الاقتراح **`);
     let channel = message.guild.channels.find(c => c.name == "suggestions");
     let embed = new Discord.RichEmbed()
-    .setFooter(`new`)    
     .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setTitle(``)
     .setFooter(`Select a reaction below to vote on suggestion`)
